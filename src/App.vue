8b2baa742670b048061a95cc31fbaf74f9947385
Искандер Абильдаев <isabil177@mail.ru>
2022-09-28 23:08:54 +0600

    first

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+
+import NavBar from "./components/NavBar.vue";
+</script>
+
+<template>
+  <div class="text-white" >
+    <nav class=" bg-[#212121] container mx-auto fixed top-0 left-0 right-0 px-6 sm:px-0 xl:max-w-7xl">
+      <div class="flex justify-between">
+        <h1 class="">Best e-comm</h1>
+        <div class="hidden justify-around basis-4/12 sm:flex">
+          <RouterLink to="/home" class="hover:text-red-500 active:text-orange-500 ">home</RouterLink>
+          <RouterLink to="/men">men</RouterLink>
+          <RouterLink to="/women">women</RouterLink>
+          <RouterLink to="/kids">kids</RouterLink>
+        </div>
+        <button class="burger sm:hidden  z-10 relative block select-none appearance-none border-none outline-none bg-none cursor-pointer duration-400 hover:span:nth-child(1):bg-red-500">
+         <span class="w-8 h-1 bg-white mb-1 relative block rounded-md"></span>
+         <span class="w-8 h-1 bg-white mb-1 relative block rounded-md hover:only:bg-red-500"></span>
+         <span class="w-8 h-1 bg-white mb-1 relative block rounded-md"></span>
+        </button>
+      </div>
+    </nav>
+
+    <RouterView />
+
+  
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,vue,html}",
+    // "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  // plugins: [require('flowbite/plugin')],
+}
